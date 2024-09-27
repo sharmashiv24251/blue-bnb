60110f9c7378860e05620b6cbd03d8b0021ce5ac
Shivansh Sharma.js <83074416+sharmashiv24251@users.noreply.github.com>
2024-09-27 23:48:01 +0530

    Update links.ts
    i hope this works

--- utils/links.ts
+++ utils/links.ts
@@ -6,9 +6,9 @@ type NavLink = {
 export const links: NavLink[] = [
   { href: "/", label: "home" },
   { href: "/favorites ", label: "favorites" },
-  { href: "/bookings ", label: "bookings" },
+//  { href: "/bookings ", label: "bookings" },
   { href: "/reviews ", label: "reviews" },
   { href: "/rentals/create ", label: "create rental" },
-  { href: "/rentals", label: "my rentals" },
+//  { href: "/rentals", label: "my rentals" },
   { href: "/profile ", label: "profile" },
 ];
